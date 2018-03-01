c406265f858fe1dad2408719226ddeaacd208f28
KidusMT <adu.kidus@yahoo.com>
2018-03-01 05:10:27 +0300

    hide the keyboard popup in registration and merged with yehualashet's repo

--- app/src/main/java/com/apps/waziup/ui/registration/RegistrationActivity.java
+++ app/src/main/java/com/apps/waziup/ui/registration/RegistrationActivity.java
@@ -1,7 +1,10 @@
 package com.apps.waziup.ui.registration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.apps.waziup.waziup.R;
 
@@ -11,5 +14,13 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        //hides the keyboard till the user selects to an edit text
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
     }
+
+//    public void toLogin(View v){
+//        startActivity(new Intent(this, ));
+//    }
 }
