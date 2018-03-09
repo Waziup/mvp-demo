b79fb5a849caeb235d38fe0d391a1743dc52f570
KidusMT <adu.kidus@yahoo.com>
2018-03-09 16:02:30 +0300

    changed the director and name for the font assets based on best practice

--- app/src/main/java/com/apps/waziup/ui/login/ResetPasswordActivity.java
+++ app/src/main/java/com/apps/waziup/ui/login/ResetPasswordActivity.java
@@ -1,12 +1,11 @@
 package com.apps.waziup.ui.login;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.apps.waziup.waziup.R;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
