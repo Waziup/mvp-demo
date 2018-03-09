dde77ca29447fc8083c958c106f8a2018025cfb7
KidusMT <adu.kidus@yahoo.com>
2018-03-09 15:28:30 +0300

    done with password reset activity, left with implementation

--- app/src/main/java/com/apps/waziup/ui/login/ResetPasswordActivity.java
+++ app/src/main/java/com/apps/waziup/ui/login/ResetPasswordActivity.java
@@ -6,19 +6,23 @@ import android.os.Bundle;
 
 import com.apps.waziup.waziup.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity {
+public class ResetPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_reset_password);
+
+        ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.login_forgot_password)
-    void onForgotPassword(){
-        startActivity(new Intent(this, ResetPasswordActivity.class));
+    @OnClick(R.id.btn_reset_password)
+    void OnBackToLogin(){
+        startActivity(new Intent(this, LoginActivity.class));
         finish();
     }
 }
