40c7374ee328188f19db0dfbf604ca4a40aeeb14
yehualashet <yehuala5355@gmail.com>
2018-04-17 15:59:37 +0300

    updating the navigation menu

--- app/src/main/java/com/apps/waziup/App.java
+++ app/src/main/java/com/apps/waziup/App.java
@@ -39,7 +39,7 @@ public class App extends MultiDexApplication implements HasActivityInjector {
 //                .build()
 //                .inject(this);
 
-        JobManagerFactory.getJobManager(this);
+//        JobManagerFactory.getJobManager(this);
     }
 
     @Override
